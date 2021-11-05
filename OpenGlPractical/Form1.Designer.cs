3c8289a7497aae33b8c7a3a30c9daac2da502b04
maszafar <masxafar@gmail.com>
2021-11-06 02:37:27 +0700

    Initial commit

---
+++ OpenGlPractical/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace OpenGlPractical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ControlWindow = new OpenTK.GLControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.drawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ControlWindow
+            // 
+            this.ControlWindow.BackColor = System.Drawing.Color.Black;
+            this.ControlWindow.Location = new System.Drawing.Point(21, 58);
+            this.ControlWindow.Margin = new System.Windows.Forms.Padding(12);
+            this.ControlWindow.Name = "ControlWindow";
+            this.ControlWindow.Size = new System.Drawing.Size(932, 856);
+            this.ControlWindow.TabIndex = 0;
+            this.ControlWindow.VSync = false;
+            this.ControlWindow.Paint += new System.Windows.Forms.PaintEventHandler(this.ControlWindow_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drawToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(974, 40);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // drawToolStripMenuItem
+            // 
+            this.drawToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.drawToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineToolStripMenuItem,
+            this.polygonToolStripMenuItem});
+            this.drawToolStripMenuItem.Name = "drawToolStripMenuItem";
+            this.drawToolStripMenuItem.Size = new System.Drawing.Size(89, 36);
+            this.drawToolStripMenuItem.Text = "Draw";
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(235, 44);
+            this.lineToolStripMenuItem.Text = "Lines";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.LineToolStripMenuItem_Click);
+            // 
+            // polygonToolStripMenuItem
+            // 
+            this.polygonToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.polygonToolStripMenuItem.Name = "polygonToolStripMenuItem";
+            this.polygonToolStripMenuItem.Size = new System.Drawing.Size(235, 44);
+            this.polygonToolStripMenuItem.Text = "Polygon";
+            this.polygonToolStripMenuItem.Click += new System.EventHandler(this.PolygonToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(72, 36);
+            this.toolStripMenuItem1.Text = "Exit";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 929);
+            this.Controls.Add(this.ControlWindow);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Practical 5";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl ControlWindow;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem drawToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polygonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ OpenGlPractical/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenGlPractical
+{
+    public partial class Form1 : Form
+    {
+        float[,] figure = new float[,]
+        {
+            { 0f, 0f },
+            { 0.3f, 0.3f },
+            { 0.5f, -0.3f },
+            { 0.29f, -0.52f },
+            { -0.52f, -0.4f },
+            { -0.4f, 0.6f }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Initilizing ControlWindow for GL on launch
+        private void ControlWindow_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Viewport(0, 0, ControlWindow.Width, ControlWindow.Height);
+            GL.ClearColor(1f, 1f, 1f, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            ControlWindow.SwapBuffers();
+        }
+
+        //Drawing X axis with ticks
+        private void DrawXAxis()
+        {
+            GL.LineWidth(1);
+            GL.Color3(1, 1, 1);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex2(0f, 1f);
+            GL.Vertex2(0f, -1f);
+            for (float i = -1f; i < 1f; i += 0.2f)
+            {
+                GL.Vertex2(i, 0.02f);
+                GL.Vertex2(i, -0.02f);
+            }
+            GL.End();
+        }
+
+        //Drawing Y axis with ticks
+        private void DrawYAxis()
+        {
+            GL.LineWidth(1);
+            GL.Color3(1, 1, 1);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex2(-1f, 0f);
+            GL.Vertex2(1f, 0f);
+            for (float i = -1f; i < 1f; i += 0.2f)
+            {
+                GL.Vertex2(0.02f, i);
+                GL.Vertex2(-0.02f, i);
+            }
+            GL.End();
+        }
+
+        //Draws a connected group of line segments.
+        private void DrawFigureLines()
+        {
+            GL.LineWidth(3);
+            GL.Color3(1, 1, 1);
+            GL.Begin(PrimitiveType.LineLoop);
+            for (int i = 0; i < 6; i++)
+            {
+                GL.Vertex2(figure[i, 0], figure[i, 1]);
+            }
+            GL.End();
+        }
+
+        //Draws a single, convex polygon.
+        private void DrawFigurePolygon()
+        {
+            Random rand = new Random();
+            GL.Begin(PrimitiveType.Polygon);
+            for (int i = 0; i < 6; i++)
+            {
+                GL.Color3(rand.NextDouble(), rand.NextDouble(), rand.NextDouble());
+                GL.Vertex2(figure[i, 0], figure[i, 1]);
+            }
+            GL.End();
+        }
+
+        //Method called when clicking on Lines in menu strip
+        private void LineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, ControlWindow.Width, ControlWindow.Height);
+            GL.ClearColor(1f, 1f, 1f, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            DrawXAxis();
+            DrawYAxis();
+            DrawFigureLines();
+            ControlWindow.SwapBuffers();
+        }
+
+        //Method called when clicking on Polyglon in menu strip
+        private void PolygonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, ControlWindow.Width, ControlWindow.Height);
+            GL.ClearColor(1f, 1f, 1f, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            DrawXAxis();
+            DrawYAxis();
+            DrawFigurePolygon();
+            ControlWindow.SwapBuffers();
+        }
+
+        //Method called when clicking on Exit in menu strip
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
